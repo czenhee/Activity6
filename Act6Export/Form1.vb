0b4a3e3328f656f63680edad2dc849f9b7ac5c8c
Czenhe <105479903+czenhee@users.noreply.github.com>
2023-05-23 17:04:17 +0800

    Add files via upload

---
+++ Act6Export/Form1.vb
@@ -0,0 +1,50 @@
+﻿Imports System.Data
+Imports Excel = Microsoft.Office.Interop.Excel
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'PayrollDataSet.PaySlip' table. You can move, or remove it, as needed.
+        Me.PaySlipTableAdapter.Fill(Me.PayrollDataSet.PaySlip)
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim xlApp As Excel.Application
+        Dim xlWorkBook As Excel.Workbook
+        Dim xlWorkSheet As Excel.Worksheet
+        Dim misValue As Object = System.Reflection.Missing.Value
+        Dim i As Integer
+        Dim j As Integer
+
+        xlApp = New Excel.Application
+        xlWorkBook = xlApp.Workbooks.Add(misValue)
+        xlWorkSheet = xlWorkBook.Sheets("sheet1")
+
+        For i = 0 To DataGridView1.RowCount - 2
+            For j = 0 To DataGridView1.ColumnCount - 1
+                xlWorkSheet.Cells(i + 1, j + 1) =
+                    DataGridView1(j, i).Value.ToString()
+            Next
+        Next
+        xlWorkSheet.SaveAs2("C:\exporteddata.xlsx")
+        xlWorkBook.Close()
+        xlApp.Quit()
+
+        releaseObject(xlApp)
+        releaseObject(xlWorkBook)
+        releaseObject(xlWorkSheet)
+        MessageBox.Show("You can find the file C:\exporteddata.xlsx")
+    End Sub
+    Private Sub releaseObject(ByVal obj As Object)
+        Try
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+            obj = Nothing
+        Catch ex As Exception
+            obj = Nothing
+        Finally
+            GC.Collect()
+        End Try
+    End Sub
+End Class
